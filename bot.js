ad4da46c49b2060c47c5cb26f0b6d45f044320ba
Matthew R <wowitbematt@gmail.com>
2018-01-23 16:46:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+const unirest = require("unirest");
 const client = new Discord.Client();
 const prefix = config.prefix;
 
